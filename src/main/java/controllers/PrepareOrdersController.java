a8a430fcf94d10ff172e1e3eec1c70733499f04c
Ricardo Guimarães <1212125@isep.ipp.pt>
2025-10-21 13:49:41 +0100

    fix UI errors on controllers

--- src/main/java/controllers/PrepareOrdersController.java
+++ src/main/java/controllers/PrepareOrdersController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import java.util.List;
+
 import domain.Warehouse;
+import dto.PrepareOrdersDTO;
 import services.PrepareOrdersService;
 
 public class PrepareOrdersController {
@@ -13,7 +16,8 @@ public class PrepareOrdersController {
         this.service = new PrepareOrdersService();
     }
 
-    public void prepareOrders() {
-        service.prepareOrders(warehouse);
+    public List<PrepareOrdersDTO> prepareOrders() {
+        return service.prepareOrders(warehouse);
     }
 }
+
