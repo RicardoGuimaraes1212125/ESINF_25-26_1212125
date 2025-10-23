35b9ccbaac72bd1e3535031b09f7c1152b67ed9a
Ricardo Guimarães <1212125@isep.ipp.pt>
2025-10-23 22:53:30 +0100

    Add AllocationRowDTO

---
+++ src/main/java/dto/AllocationRowDTO.java
@@ -0,0 +1,40 @@
+package dto;
+
+public class AllocationRowDTO {
+
+    private final String orderId;
+    private final int lineNo;
+    private final String sku;
+    private final int qty;
+    private final String boxId;
+    private final String warehouseId;
+    private final int aisle;
+    private final int bayNumber;
+
+    public AllocationRowDTO(String orderId, int lineNo, String sku, int qty,
+                            String boxId, String warehouseId, int aisle, int bayNumber) {
+        this.orderId = orderId;
+        this.lineNo = lineNo;
+        this.sku = sku;
+        this.qty = qty;
+        this.boxId = boxId;
+        this.warehouseId = warehouseId;
+        this.aisle = aisle;
+        this.bayNumber = bayNumber;
+    }
+
+    public String getOrderId() { return orderId; }
+    public int getLineNo() { return lineNo; }
+    public String getSku() { return sku; }
+    public int getQty() { return qty; }
+    public String getBoxId() { return boxId; }
+    public String getWarehouseId() { return warehouseId; }
+    public int getAisle() { return aisle; }
+    public int getBayNumber() { return bayNumber; }
+
+    @Override
+    public String toString() {
+        return String.format("Order %s | Line %d | SKU %s | Qty %d | Box %s | Bay %s/%d/%d",
+                orderId, lineNo, sku, qty, boxId, warehouseId, aisle, bayNumber);
+    }
+}
