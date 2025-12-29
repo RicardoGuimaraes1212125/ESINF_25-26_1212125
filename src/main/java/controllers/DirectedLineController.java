4ee6099d90def30498a55c3b1f6ee969b05cf3d9
Ricardo Guimarães <1212125@isep.ipp.pt>
2025-12-29 17:42:14 +0000

    Update Controller

--- src/main/java/controllers/DirectedLineController.java
+++ src/main/java/controllers/DirectedLineController.java
@@ -4,12 +4,6 @@ import graph.Graph;
 import services.DirectedLineService;
 import dto.DirectedLineResultDTO;
 
-/*
-* Controller for the Directed Line Upgrade Plan functionality.
-* It interacts with the DirectedLineService to process the graph
-* and returns the result encapsulated in a DirectedLineResultDTO.
-*/
-
 public class DirectedLineController {
 
     private final DirectedLineService service = new DirectedLineService();
@@ -18,3 +12,4 @@ public class DirectedLineController {
         return service.computeUpgradePlan(graph);
     }
 }
+
