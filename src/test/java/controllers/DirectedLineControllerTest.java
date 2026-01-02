3ee2609ad31e079a666ada1c6b245b4c864e03bc
Ricardo Guimarães <1212125@isep.ipp.pt>
2026-01-02 18:05:29 +0000

    update controller test us11

--- src/test/java/controllers/DirectedLineControllerTest.java
+++ src/test/java/controllers/DirectedLineControllerTest.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import domain.RailLine;
+import domain.RailNode;
 import dto.DirectedLineResultDTO;
 import graph.Graph;
 import graph.map.MapGraph;
@@ -9,19 +11,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DirectedLineControllerTest {
 
+    private RailNode n(String id) {
+        return new RailNode(id, "Station " + id, 0, 0, 0, 0);
+    }
+
     @Test
     void controllerShouldHandleMediumGraph() {
 
-        Graph<String, Double> g = new MapGraph<>(true);
+        Graph<RailNode, RailLine> g = new MapGraph<>(true);
 
-        g.addEdge("S1", "S2", 5.0);
-        g.addEdge("S2", "S3", 3.0);
-        g.addEdge("S3", "S4", 2.0);
-        g.addEdge("S1", "S5", 7.0);
-        g.addEdge("S5", "S6", 1.0);
+        RailNode s1 = n("S1");
+        RailNode s2 = n("S2");
+        RailNode s3 = n("S3");
+        RailNode s4 = n("S4");
+        RailNode s5 = n("S5");
+        RailNode s6 = n("S6");
 
-        DirectedLineController controller = new DirectedLineController();
-        DirectedLineResultDTO result = controller.execute(g);
+        g.addEdge(s1, s2, new RailLine("S1", "S2", 5, 0, 0));
+        g.addEdge(s2, s3, new RailLine("S2", "S3", 3, 0, 0));
+        g.addEdge(s3, s4, new RailLine("S3", "S4", 2, 0, 0));
+        g.addEdge(s1, s5, new RailLine("S1", "S5", 7, 0, 0));
+        g.addEdge(s5, s6, new RailLine("S5", "S6", 1, 0, 0));
+
+        DirectedLineResultDTO result =
+                new DirectedLineController().execute(g);
 
         assertFalse(result.hasCycle());
         assertEquals(6, result.getUpgradeOrder().size());
@@ -30,11 +43,15 @@ class DirectedLineControllerTest {
     @Test
     void controllerShouldReturnCycleInformation() {
 
-        Graph<String, Double> g = new MapGraph<>(true);
+        Graph<RailNode, RailLine> g = new MapGraph<>(true);
 
-        g.addEdge("X", "Y", 1.0);
-        g.addEdge("Y", "Z", 1.0);
-        g.addEdge("Z", "X", 1.0);
+        RailNode x = n("X");
+        RailNode y = n("Y");
+        RailNode z = n("Z");
+
+        g.addEdge(x, y, new RailLine("X", "Y", 1, 0, 0));
+        g.addEdge(y, z, new RailLine("Y", "Z", 1, 0, 0));
+        g.addEdge(z, x, new RailLine("Z", "X", 1, 0, 0));
 
         DirectedLineResultDTO result =
                 new DirectedLineController().execute(g);
@@ -43,4 +60,3 @@ class DirectedLineControllerTest {
         assertEquals(3, result.getCycleStations().size());
     }
 }
-
