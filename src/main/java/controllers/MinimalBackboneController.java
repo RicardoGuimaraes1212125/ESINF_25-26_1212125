b6bd055bf3b373180b203340e8aef05f334fe525
Ricardo Guimarães <1212125@isep.ipp.pt>
2026-01-02 17:59:12 +0000

    added controller to minimal backbone

--- src/main/java/controllers/MinimalBackboneController.java
+++ src/main/java/controllers/MinimalBackboneController.java
@@ -1,18 +1,15 @@
 package controllers;
 
+import domain.RailLine;
+import domain.RailNode;
 import graph.Graph;
 import services.MinimalBackboneService;
 
 public class MinimalBackboneController {
 
-    private final MinimalBackboneService service;
+    private final MinimalBackboneService service = new MinimalBackboneService();
 
-    public MinimalBackboneController() {
-        this.service = new MinimalBackboneService();
-    }
-
-    public Graph<String, Double> computeMinimalBackbone(Graph<String, Double> graph) {
+    public Graph<RailNode, RailLine> computeMinimalBackbone(Graph<RailNode, RailLine> graph) {
         return service.computeMinimalBackbone(graph);
     }
 }
-
