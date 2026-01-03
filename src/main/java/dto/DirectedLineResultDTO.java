c43352d36fc6e9f8c60d0dc072aba3bd6969be37
Ricardo Guimarães <1212125@isep.ipp.pt>
2026-01-03 17:22:20 +0000

    Added CycleCount and Rail Lines to show on UI

--- src/main/java/dto/DirectedLineResultDTO.java
+++ src/main/java/dto/DirectedLineResultDTO.java
@@ -1,6 +1,7 @@
 package dto;
 
 import domain.RailNode;
+import domain.RailLine;
 
 import java.util.List;
 import java.util.Set;
@@ -13,16 +14,22 @@ public class DirectedLineResultDTO {
     private final boolean hasCycle;
     private final List<RailNode> upgradeOrder;
     private final Set<RailNode> cycleStations;
+    private final Set<RailLine> cycleLinks;
+    private final int cycleCount;
     private final String complexity;
 
     public DirectedLineResultDTO(boolean hasCycle,
                                  List<RailNode> upgradeOrder,
                                  Set<RailNode> cycleStations,
+                                 Set<RailLine> cycleLinks,
+                                 int cycleCount,
                                  String complexity) {
 
         this.hasCycle = hasCycle;
         this.upgradeOrder = upgradeOrder;
         this.cycleStations = cycleStations;
+        this.cycleLinks = cycleLinks;
+        this.cycleCount = cycleCount;
         this.complexity = complexity;
     }
 
@@ -36,6 +43,14 @@ public class DirectedLineResultDTO {
 
     public Set<RailNode> getCycleStations() {
         return cycleStations;
+    }
+
+    public Set<RailLine> getCycleLinks() {
+        return cycleLinks;
+    }
+
+    public int getCycleCount() {
+        return cycleCount;
     }
 
     public String getComplexity() {
