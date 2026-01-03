6fad2e2629d7c402375618bb472ed0edc3da7eb2
Ricardo Guimarães <1212125@isep.ipp.pt>
2026-01-03 17:24:26 +0000

    udpate on service to perform after find one cycle

--- src/main/java/services/DirectedLineService.java
+++ src/main/java/services/DirectedLineService.java
@@ -20,21 +20,38 @@ public class DirectedLineService {
         Deque<RailNode> stack = new ArrayDeque<>();
         List<RailNode> topoOrder = new ArrayList<>();
         Set<RailNode> cycleStations = new LinkedHashSet<>();
+        Set<RailLine> cycleLinks = new LinkedHashSet<>();
+        int[] cycleCount = {0}; // Array para permitir modificação em método auxiliar
 
         Arrays.fill(color, Color.WHITE);
 
         for (RailNode v : graph.vertices()) {
-            int k = graph.key(v);
-            if (k >= 0 && color[k] == Color.WHITE) {
-                if (dfs(graph, v, color, stack, topoOrder, cycleStations)) {
-                    return new DirectedLineResultDTO(
-                            true,
-                            null,
-                            cycleStations,
-                            "O(V + E)"
-                    );
-                }
+            int key = graph.key(v);
+
+            if (key >= 0 && color[key] == Color.WHITE) {
+                dfs(
+                        graph,
+                        v,
+                        color,
+                        stack,
+                        topoOrder,
+                        cycleStations,
+                        cycleLinks,
+                        cycleCount,
+                        null
+                );
             }
+        }
+
+        if (!cycleStations.isEmpty()) {
+            return new DirectedLineResultDTO(
+                    true,
+                    null,
+                    cycleStations,
+                    cycleLinks,
+                    cycleCount[0],
+                    "O(V + E)"
+            );
         }
 
         Collections.reverse(topoOrder);
@@ -43,50 +60,92 @@ public class DirectedLineService {
                 false,
                 topoOrder,
                 Collections.emptySet(),
+                Collections.emptySet(),
+                0,
                 "O(V + E)"
         );
     }
 
-    private boolean dfs(Graph<RailNode, RailLine> graph,
-                        RailNode u,
-                        Color[] color,
-                        Deque<RailNode> stack,
-                        List<RailNode> topoOrder,
-                        Set<RailNode> cycleStations) {
+    private void dfs(Graph<RailNode, RailLine> graph,
+                     RailNode u,
+                     Color[] color,
+                     Deque<RailNode> stack,
+                     List<RailNode> topoOrder,
+                     Set<RailNode> cycleStations,
+                     Set<RailLine> cycleLinks,
+                     int[] cycleCount,
+                     RailLine incomingEdge) {
 
         int uKey = graph.key(u);
         color[uKey] = Color.GRAY;
         stack.push(u);
 
         for (Edge<RailNode, RailLine> e : graph.outgoingEdges(u)) {
+
             RailNode v = e.getVDest();
             int vKey = graph.key(v);
 
-            if (color[vKey] == Color.GRAY) {
-                extractCycle(v, stack, cycleStations);
-                return true;
+            // Ciclo detectado (back-edge)
+            if (color[vKey] == Color.GRAY && !cycleStations.contains(v)) {
+                extractCycle(v, stack, cycleStations, cycleLinks, graph, e.getWeight());
+                cycleCount[0]++;
             }
 
+            // Exploração normal
             if (color[vKey] == Color.WHITE) {
-                if (dfs(graph, v, color, stack, topoOrder, cycleStations)) {
-                    return true;
-                }
+                dfs(graph, v, color, stack, topoOrder, cycleStations, cycleLinks, cycleCount, e.getWeight());
             }
         }
 
         stack.pop();
         color[uKey] = Color.BLACK;
-        topoOrder.add(u);
-        return false;
+        
+        if (cycleStations.isEmpty()) {
+            topoOrder.add(u);
+        }
     }
 
     private void extractCycle(RailNode start,
                               Deque<RailNode> stack,
-                              Set<RailNode> cycleStations) {
+                              Set<RailNode> cycleStations,
+                              Set<RailLine> cycleLinks,
+                              Graph<RailNode, RailLine> graph,
+                              RailLine closingEdge) {
 
-        for (RailNode v : stack) {
-            cycleStations.add(v);
-            if (v.equals(start)) break;
+        List<RailNode> path = new ArrayList<>(stack);
+        int startIdx = -1;
+
+        // Encontra o índice de 'start' na pilha
+        for (int i = 0; i < path.size(); i++) {
+            if (path.get(i).equals(start)) {
+                startIdx = i;
+                break;
+            }
+        }
+
+        if (startIdx >= 0) {
+            // Adiciona todos os nós do ciclo
+            for (int j = 0; j <= startIdx; j++) {
+                cycleStations.add(path.get(j));
+            }
+
+            // Adiciona todas as arestas do ciclo
+            for (int j = 0; j < startIdx; j++) {
+                RailNode from = path.get(j);
+                RailNode to = path.get(j + 1);
+
+                // Encontra a aresta entre from e to
+                for (Edge<RailNode, RailLine> e : graph.outgoingEdges(from)) {
+                    if (e.getVDest().equals(to)) {
+                        cycleLinks.add(e.getWeight());
+                        break;
+                    }
+                }
+            }
+
+            // Adiciona a aresta de fechamento do ciclo
+            cycleLinks.add(closingEdge);
         }
     }
 }
+
